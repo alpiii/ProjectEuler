f3c377497481637ee8f50445a425946a95c9e262
Hasan Alper Ocalan <haocalan@gmail.com>
2016-09-29 23:38:18 +0300

    Question 9 solved. Quadratic Equations Method is used to generate Pythagorean triples. Sum of each element of each triple is compared with the sum (1000 in question).

---
+++ src/Question9.java
@@ -0,0 +1,46 @@
+
+/*
+ * Special Pythagorean triplet
+Problem 9
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+ * 
+ */
+
+public class Question9 {
+
+	public static void main(String[] args) {
+		int[] triple = PythagoreanTriples(1000);
+		if (triple != null) {
+			System.out.println("Triple : " + triple[0] + ", " + triple[1] + ", " + triple[2]);
+			System.out.println("Result (a*b*c) : " + triple[0] * triple[1] * triple[2]);
+		} else {
+			System.out.println("No triple found!");
+		}
+	}
+
+	private static int[] PythagoreanTriples(int sum) {
+		// Quadratic Equations Method to generate Pythagorean Triples
+		int a = 0, b = 0, c = 0, m = 2;
+
+		while (m <= sum) {
+			for (int n = 1; n < m; n++) {
+				a = m * m - n * n;
+				b = 2 * m * n;
+				c = m * m + n * n;
+				// we know that sum of the triple numbers
+				// are value of sum variable
+				if (a + b + c == sum) {
+					return new int[] { a, b, c };
+				}
+			}
+			m++;
+		}
+		return null;
+	}
+}
