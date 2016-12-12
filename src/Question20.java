27c708c6aa4de1cb9f759100bc6c0ed54be4f535
Hasan Alper Ocalan <haocalan@gmail.com>
2016-12-12 18:45:03 +0200

    Question 20 solved.

---
+++ src/Question20.java
@@ -0,0 +1,29 @@
+
+/*
+ * Factorial digit sum
+Problem 20
+n! means n x (n - 1) x ... x 3 x 2 x 1
+
+For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800,
+and the sum of the digits in the 
+number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+ * 
+ */
+
+import EulerHelper.BigNumbers;
+
+public class Question20 {
+
+	public static void main(String[] args) {
+		int i = 100;
+		String result = BigNumbers.bigFactorial(i);
+		int count = 0;
+		for (int j = 0; j < result.length(); j++) {
+			count += Integer.valueOf(result.substring(j, j + 1));
+		}
+		System.out.println("Result : " + count);
+	}
+
+}
