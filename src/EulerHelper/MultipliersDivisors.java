e8672191c3edbf355fc569af0b5fb95289b2155a
Hasan Alper Ocalan <haocalan@gmail.com>
2016-12-25 23:31:34 +0200

    Added factorial method.

--- src/EulerHelper/MultipliersDivisors.java
+++ src/EulerHelper/MultipliersDivisors.java
@@ -45,4 +45,11 @@ public class MultipliersDivisors {
 			return divisors;
 		}
 	}
+
+	public static int factorial(int num) {
+		if (num <= 1)
+			return 1;
+		else
+			return num * factorial(num - 1);
+	}
 }
