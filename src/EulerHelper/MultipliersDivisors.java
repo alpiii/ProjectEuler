ef9a265cdb009d107bbb36efd5d2a60432cc6c45
alpiii <haocalan@gmail.com>
2019-08-05 23:31:13 +0300

    Question 33 solved.

--- src/EulerHelper/MultipliersDivisors.java
+++ src/EulerHelper/MultipliersDivisors.java
@@ -52,4 +52,15 @@ public class MultipliersDivisors {
 		else
 			return num * factorial(num - 1);
 	}
+	
+	public static int gcd(int num1, int num2) {
+		while(num1 != num2)
+        {
+            if(num1 > num2)
+                num1 -= num2;
+            else
+                num2 -= num1;
+        }
+		return num1;
+	}
 }
